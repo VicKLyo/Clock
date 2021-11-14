740d73385e9e883a0bb4e910310c8de7d4705020
VicKLyo <91342885+VicKLyo@users.noreply.github.com>
2021-11-14 13:33:30 +0200

    Update app.js

--- app.js
+++ app.js
@@ -10,12 +10,12 @@ async function currentTime(mins, currentQuote) {
    const min = date.getMinutes()   
    // if there is no current quote, get one
    if (currentQuote === undefined) currentQuote = await randomQuote()
-   // when the time has changed, update! The first time this function fires, `mins` is undefined and thus won't equal `min`
+  
    if (min !== mins) {
      const greeting = date.getHours() > 11 ? "afternoon" : "morning"
-     // using the `Intl` object, we can get the time format a lot easier!
+  
      let currentDate = new Intl.DateTimeFormat('en-US',{hour12: false, timeStyle: "short"}).format(date)
-     // using string literals is easier to read than string concatination
+   
      greeter.textContent = `Good ${greeting}, It's currently`
      timeContainer.textContent = currentDate
      quote.textContent = currentQuote
